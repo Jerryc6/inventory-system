b65c2c01fa085183944429e85fed711ad7c781dc
Jerryc6 <jerryclanuevo27@gmail.com>
2025-11-25 06:49:36 -0500

    start creating JFrame

--- main.java
+++ main.java
@@ -1,1 +1,33 @@
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class main{
+
+	public void Object(){
+
+		JFrame frame = new JFrame("Screen");
+		frame.setSize(500,600);
+		frame.setLayout(null);
+		frame.setVisible(true);
+
+		JButton bt = new JButton("PRESS");
+		frame.add(bt);
+		bt.setBounds(150,200,220,50);
+
+		bt.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e){	
+				System.out.println("Hello");
+			}
+		});
+	}
+
+
+
+	public static void main(String [] args){
+		main m = new main();
+		m.Object();
+	}
+}
