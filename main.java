02042a0fd1d0c84b3028228e13b82b19238b3889
Jerryc6 <jerryclanuevo27@gmail.com>
2025-11-30 08:40:01 -0500

    start working in #1

--- main.java
+++ main.java
@@ -1,33 +1,54 @@
-import java.io.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+
+import javax.swing.JFrame;
+import java.util.Scanner;
 
 public class main{
 
-	public void Object(){
-
-		JFrame frame = new JFrame("Screen");
-		frame.setSize(500,600);
-		frame.setLayout(null);
+	public static void SoftwareJFrame(){ //This is the main frame of software
+		JFrame frame = new JFrame("Invenory-System");
+		frame.setSize(1200, 700);
 		frame.setVisible(true);
-
-		JButton bt = new JButton("PRESS");
-		frame.add(bt);
-		bt.setBounds(150,200,220,50);
-
-		bt.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e){	
-				System.out.println("Hello");
-			}
-		});
+		frame .setLayout(null);
 	}
 
+	public static void RI(){ // #1 Receiving Inventory
+		
+		Scanner RIsc = new Scanner(System.in);
+
+		System.out.println("Enter company:");
+		String company = RIsc.nextLine();
+
+		System.out.println("Enter supplier:");
+		String supplier = RIsc.nextLine();
+		
+		System.out.println("Enter purchase order:");
+		String purchaseOrder = RIsc.nextLine();
+
+		try{
+			if(company.equals("abc company")){
+				System.out.println("Correct");
+			}if(supplier.equals("xyz supplier")){
+				System.out.println("Correct");
+			}if(purchaseOrder.equals("code")){
+				System.out.println("Correct");
+			}
+		
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+	public static void SOI(){ // #2. Storing & Organizing Inventory
+
+	}
+
+	public static void RTIT(){// #3 Real-Time Inventory Tracking
+
+	}
 
 
 	public static void main(String [] args){
 		main m = new main();
-		m.Object();
+		m.RI();
 	}
 }
