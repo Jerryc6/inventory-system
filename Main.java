a76d53a04a3f52d2cf0c63ee5b3781a322543d57
Jerryc6 <jerryclanuevo27@gmail.com>
2025-12-04 10:14:21 -0500

    fixing folders

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+
+
+	public static void RI(){ // #1 Receiving Inventory
+	}
+
+	public static void SOI(){ // #2. Storing & Organizing Inventory
+	}
+
+
+	public static void RTIT(){// #3 Real-Time Inventory Tracking
+	}
+
+	public static void main(String [] args){
+		
+
+	}
+}
